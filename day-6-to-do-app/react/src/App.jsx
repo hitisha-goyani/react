9dcbf4e6bfc76bc67e23387d647caf60e74bc687
unknown <hitisha-goyani>
2025-05-12 23:11:38 +0530

    edit to-do-app

--- day-6-to-do-app/react/src/App.jsx
+++ day-6-to-do-app/react/src/App.jsx
@@ -7,20 +7,28 @@ function App() {
 
        const [todo,setTodo] = useState([])
        const[to,setTO] =useState("")
+      const [editid, setEditId] = useState("")
 
        function handleTodo(){
 
-        let obj = {
+           if (editid !== "") {
 
-          id:Math.round(Math.random()*1000),
-          task : to,
-          status : false
+          let updatedtodo = todo.map((ele) =>ele.id === editid ? { ...ele, task: to } : ele
+      )
+      setTodo(updatedtodo)
+      setEditId("")
 
-        }
-
-        setTodo([...todo,obj])
+    } 
+    else {
+      let obj = {
+        id: Math.round(Math.random() * 1000),
+        task: to,
+        status: false,
+      }
+      setTodo([...todo, obj])
+    }
+    setTO("")
        }
-
 
        function handleUpdate(id){
         let newtodo = todo.map((ele)=>ele.id == id ? {...ele , status :!ele.status} : ele)
@@ -32,6 +40,13 @@ function App() {
           let filtertodo= todo.filter((ele) => ele.id !== id)
           setTodo(filtertodo)
   }
+    function handleEdit(id) {
+    let taskedit = todo.find((ele) => ele.id === id)
+    setTO(taskedit.task)
+    setEditId(id)
+  }
+  
+
 
 
   return (
@@ -39,7 +54,9 @@ function App() {
     <h1 className='font-bold text-center text-3xl'>To-do-app</h1>
       <div className='w-5xl m-auto border flex flex-col gap-4 p-4 my-5'>
         <input className='border ' type="text" onChange={(e)=> setTO(e.target.value)} placeholder='enter work .......'/>
-        <button onClick={handleTodo} className='border bg-gray-400 w-20 p-2 rounded-2xl'>add</button>
+         <button onClick={handleTodo} className='border bg-gray-400 w-24 p-2 rounded-2xl'>  {editid !== "" ? "Update" : "Add"}</button>
+        
+        
       </div>
 
       <div className='border w-5xl my-5 gap-5 flex flex-col mx-auto p-3 rounded-2xl bg-blue-300'>
@@ -47,10 +64,11 @@ function App() {
           todo.map((ele) =>
           (
               <div key={ele.id} className='flex gap-5'>
-
+                
               <h3 className='font-bold'>{ele.task}</h3>
               <button className='bg-indigo-400 p-2 rounded-md ' onClick={()=> handleUpdate(ele.id)}>{ele.status ? "completed" :  "not completed"} </button>
               <button  className='bg-red-500 p-2 rounded-md ' onClick={()=>handleDelete(ele.id)}>delete</button>
+                <button className='bg-yellow-400 p-2 rounded-md'onClick={() => handleEdit(ele.id)}>Edit</button>
               </div>
           ))
         }
